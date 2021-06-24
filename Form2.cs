cdc61b89033ae0cecb664b0953e5d5a5fa8583c7
vkorootaev1 <84430062+White-Frost@users.noreply.github.com>
2021-06-24 17:39:42 +0500

    form_2 finish

---
+++ Form2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection sqlcon;
+        SqlCommand command;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                sqlcon = new SqlConnection("Data Source=sql5053.site4now.net;Initial Catalog=db_a765d1_stud;User ID=db_a765d1_stud_admin;Password=2948996John");
+                sqlcon.Open();
+                command = new SqlCommand("SELECT COUNT (Login) FROM [Curator] WHERE Login =@Login AND Password = @Password", sqlcon);
+                command.Parameters.AddWithValue("Login", textBox1.Text);
+                command.Parameters.AddWithValue("Password", maskedTextBox1.Text);
+                int count = (int)command.ExecuteScalar();
+                command.ExecuteNonQuery();
+                sqlcon.Close();
+            if (count == 1 || (textBox1.Text ==Program.login_admin && maskedTextBox1.Text == Program.pass_admin))
+            {
+                if(textBox1.Text == Program.login_admin && maskedTextBox1.Text == Program.pass_admin)
+                {
+                    Program.f = true;
+                }
+                Program.login= int.Parse(textBox1.Text); 
+                this.Hide();
+                Form1 forms = new Form1();
+                forms.Show();
+            }
+            else
+            {
+                MessageBox.Show("Неправильный логин или пароль!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Program.f = false;
+            this.ControlBox = false;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0' && e.KeyChar <= '9')  || e.KeyChar == (char)Keys.Back)
+            {
+
+            }
+            else
+            {
+                e.Handled = true;
+                MessageBox.Show("Используйте только арабские цифры при вводе логина", "Ошибка");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    static class Program
+    {
+        public static string login_admin = "1111";
+        public static string pass_admin = "2110";
+        public static bool f;
+        public static int login;
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form2());
+        }
+    }
+}
